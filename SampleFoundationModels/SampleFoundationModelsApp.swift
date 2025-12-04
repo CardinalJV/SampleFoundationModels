9502e920dcda52ee3507a647baf0f73a9b0b9a6d
CardinalJV <129071629+CardinalJV@users.noreply.github.com>
2025-12-04 17:13:22 +0100

    Initial Commit

---
+++ SampleFoundationModels/SampleFoundationModelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleFoundationModelsApp.swift
+//  SampleFoundationModels
+//
+//  Created by Viranaiken Jessy on 04/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleFoundationModelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
